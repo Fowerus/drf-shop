bfb8b1cdbeaea6260292cd30ebdd25583acfbbf3
Fowerus <fowerus@gmail.com>
2023-01-16 17:52:29 +0300

    Initial branch commit

--- backend/Users/urls.py
+++ backend/Users/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from . import views 
+
 
 
 urlpatterns = [
-	path('registration/', views.UserRegistrationAPIView.as_view(), name = 'user-registration'),
-	path('login/', views.UserLoginAPIView.as_view(), name = 'user-login'),
-	path('retrieve/<int:user_id>/', views.UserRetrieveUpdateAPIView.as_view(), name = 'user-retrieve'),
-	path('update/<int:user_id>/', views.UserRetrieveUpdateAPIView.as_view(), name = 'user-update'),
-
-	path('check-token/',views.CheckTokenAPIView.as_view(), name = 'user-check-token')
+	
 ]
